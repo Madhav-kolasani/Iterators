a332818bf048a69ee08276c3f39a65ba21b877f9
Madhav <madhavkolasani@gmail.com>
2025-02-23 15:58:46 +0530

    nw

--- app.py
+++ app.py
@@ -205,15 +205,15 @@ if __name__ == '__main__':
     app.run(debug=True)
 
 
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder='.')
 
 @app.route('/')
 def home():
-    return "Flask App is Running!"
+    return render_template('index.html')
 
-if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 8080))  # Read PORT from Railway
-    app.run(host='0.0.0.0', port=port, debug=False)  # Disable Debug mode
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
